d2efd0da8da23396ce8d105f50cda8379f4d06c1
babanayak <jarupulababa@gmail.com>
2020-05-20 05:47:45 +0530

    Initial commit

---
+++ src/main/java/com/EstimatedDeliveryDate/EstimatedDeliveryDateApplication.java
@@ -0,0 +1,14 @@
+package com.EstimatedDeliveryDate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstimatedDeliveryDateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstimatedDeliveryDateApplication.class, args);
+
+	}
+
+}
